410c18488e62abbbce5d20e72c6dbf8ef1f33891
R_Skvirskiy <rskvirskiy@gmail.com>
2012-07-12 03:34:23 +0200

    Added New Test With Mock
    добавлен тест с ипользованием Mock, но пока что не рабочий.

---
+++ PolyTeam.Calculator.Test/OutputInput_Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+
+namespace PolyTeam.Calculator.Test
+{
+    [TestFixture]
+    public class OutputInput_Test
+    {
+        [Test]
+        public void Should_ConsoleOutCalledAtOnce_WhenGenericOutCalled()
+        {
+            //arrenge
+            var MockConsoleOutput = new Mock<ICalculatorOutput>();
+            var Distributor = new CalculatorDistributor(MockConsoleOutput.Object);
+            //act
+            Distributor.GenericOutput(0);
+            //assert
+            MockConsoleOutput.Verify(x => x.Out(0),Times.Once());
+        }
+
+    }
+}
